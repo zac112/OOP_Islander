681b6f007157fa8bed361c029ae1ac7a7466b9b5
Kimi Heinonen <kimiheinonen@hotmail.com>
2019-03-02 17:22:46 +0200

    Lisatty scriptit city ja resourcepool

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    
+    /** 
+     *  Kayttaa resurssia r, maaran 'amount' verran (amount >= 0).
+     *  Voidaan kayttaa maximissaan sen verran resursseja mita niita on kaytossa.
+     */
+    public void useResource(Resource r, int amount)
+    {
+        if (amount<0)
+        {
+            return;
+        }
+        ResourcePool[] lista = gameObject.GetComponents<ResourcePool>();
+        for (int i = 0; i < lista.Length; i++)
+        {
+            if (lista[i].getResource().GetType().Equals(r.GetType()))
+            {
+                lista[i].useResource(amount);
+            }
+        }
+    }
+
+    /** 
+*  Lisaa resurssia r, maaran 'amount' verran (amount >= 0).
+*  Voidaan lisata maximissaan resurssin maximikapasiteettiin asti,
+*  ts. jos nykyinen resurssimaara + lisatty maara >= maximikapasiteetti
+*  niin uusi resurssimaara = maximikapasiteetti
+*/
+    public void addResource(Resource r, int amount)
+    {
+        if (amount<0)
+        {
+            return;
+        }
+        ResourcePool[] lista = gameObject.GetComponents<ResourcePool>();
+       
+        for (int i = 0; i < lista.Length; i++)
+        {
+            if (lista[i].getResource().GetType().Equals(r.GetType()))
+            {
+                lista[i].addResource(amount);
+                return;
+            }
+        }
+        ResourcePool respool = gameObject.AddComponent<ResourcePool>();
+        respool.setResource(r);
+        respool.addResource(amount);
+    }
+}
---
+++ Assets/Scripts/ResourcePool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePool : MonoBehaviour
+{
+
+    [SerializeField]
+    private Resource r;
+
+    [SerializeField]
+    private int amount = 0;
+
+    [SerializeField]
+    private int maxCapacity = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    public void setResource(Resource r)
+    {
+        this.r = r;
+    }
+
+    public Resource getResource()
+    {
+        return r;
+    }
+    /**
+     * Kayttaa resurssia maaran X verran
+     */
+    public void useResource(int x)
+    {
+        if (this.amount - x <= 0)
+        {
+            this.amount = 0;
+        }
+        else
+        {
+            this.amount -= x;
+        }
+    }
+    /**
+     * Lisaa resurssia maaran X verran
+     */
+    public void addResource(int x)
+    {
+        if (this.amount + x >= this.maxCapacity)
+        {
+            this.amount = this.maxCapacity;
+        }
+        else
+        {
+            this.amount += x;
+        }
+    }
+}
