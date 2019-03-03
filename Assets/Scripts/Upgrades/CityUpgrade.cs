18a98dd074a5f0a7249e70e502e7d546089a3cca
Matti Loimaranta <makalo@utu.fi>
2019-03-03 11:47:50 +0200

    added city level upgrade

---
+++ Assets/Scripts/Upgrades/CityUpgrade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityUpgrade : Upgrade
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpgradeCity()
+    {
+
+        UpgradeTargets target = UpgradeTargets.city;
+        City city = gameObject.GetComponent<City>();
+        int currentLevel = city.GetLevel(target);
+
+        if (city.GetLevel(UpgradeTargets.population) >= 5) {
+            city.UseResources(GetPrices(currentLevel * 10));
+        //    city.IncreaseCityLevel();  currently not implemented in City
+        }
+        
+    }
+}
