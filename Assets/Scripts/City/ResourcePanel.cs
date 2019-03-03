af42a2efdc6fff2a1e48ec87238bd5138ab6e896
Daniel Sundholm <dajsun@utu.fi>
2019-03-03 19:35:26 +0200

    Resurssipalkit viimeinkin, 4 tuntia nyt työstäny, ei meinaa jaksaa enää, jatkan myöhemmin: eli karkeet versiot

---
+++ Assets/Scripts/City/ResourcePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePanel : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResourcePool[] lista = gameObject.GetComponents<ResourcePool>();
+        for (int i = 0; i < lista.Length; i++)
+        {
+            UpdatePanel(lista[i]);
+        }
+    }
+
+    public void UpdatePanel(ResourcePool respool)
+    {
+
+    }
+}
---
+++ Assets/Scripts/City/ResourceRow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceRow : MonoBehaviour
+{
+    private Resource r;
+
+    private Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetRow(Resource r, float x, float y, float z)
+    {
+        this.r = r;
+        this.GetComponent<SpriteRenderer>().sprite = r.getResourceImg();
+        this.GetComponent<SpriteRenderer>().transform.position = new Vector3(x, y, z);
+        this.GetComponent<SpriteRenderer>().transform.localScale = new Vector3(0.35f, 0.35f, 1f);
+    }
+
+}
