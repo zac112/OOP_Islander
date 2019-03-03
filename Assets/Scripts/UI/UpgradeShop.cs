f671c3e28cd26c667fb91d7dd0a7069ea183ba1b
Matti Loimaranta <makalo@utu.fi>
2019-03-03 18:37:12 +0200

    added upgrade shop

---
+++ Assets/Scripts/UI/UpgradeShop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeShop : MonoBehaviour
+{ 
+    private bool shopVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopVisible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        //Debug.Log("Kauppa auki");
+    }
+}
