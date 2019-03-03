404da79ed6a1356fd598edeedeaf5ec3276dfc64
Kimi Heinonen <kimiheinonen@hotmail.com>
2019-03-04 00:07:00 +0200

    palkit paivittyy oikein

--- Assets/Scripts/City/ResourceRow.cs
+++ Assets/Scripts/City/ResourceRow.cs
@@ -10,7 +10,10 @@ public class ResourceRow : MonoBehaviour
 
     [SerializeField]
     private Sprite[] sprites;
-    
+
+    [SerializeField]
+    private Sprite[] upgradesprites;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +32,7 @@ public class ResourceRow : MonoBehaviour
         this.r = r;
         this.GetComponent<SpriteRenderer>().sprite = r.getResourceImg();
         this.GetComponent<SpriteRenderer>().transform.position = new Vector3(x, y, z);
-        this.GetComponent<SpriteRenderer>().transform.localScale = new Vector3(0.35f, 0.35f, 1f);
+        //this.GetComponent<SpriteRenderer>().transform.localScale = new Vector3(0.35f, 0.35f, 1f);
     }
     public void UpdateBars()
     {
@@ -40,29 +43,72 @@ public class ResourceRow : MonoBehaviour
         {
             if (lista[i].GetResource().GetType().Equals(r.GetType()))
             {
-                
-
                 int amount = lista[i].GetAmount();
                 int max = lista[i].GetMaxCapacity();
                 float pros = amount*1f / max*1f;
-                if (pros<=0.2)
+                if (max<110)
                 {
-                    transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[0];
-                } else if(pros<=0.4)
-                {
-                    transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[1];
-                } else if(pros<=0.6)
-                {
-                    transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[2];
-                } else if(pros<=0.8)
-                {
-                    transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[3];
+                    SetSpritesForLevel1(pros);
                 } else
                 {
-                    transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[4];
+                    SetSpritesForLevel2(pros);
                 }
+                
             }
         }
     }
+    private void SetSpritesForLevel1(float pros)
+    {
+        if (pros <= 0.125)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        else if (pros <= 0.375)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+        else if (pros <= 0.625)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[2];
+        }
+        else if (pros <= 0.875)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[3];
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sprites[4];
+        }
+        
+    }
+    private void SetSpritesForLevel2(float pros)
+    {
+        if (pros <= 0.167)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[0];
+        }
+        else if (pros <= 0.333)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[1];
+        }
+        else if (pros <= 0.5)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[2];
+        }
+        else if (pros <= 0.667)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[3];
+        }
+        else if (pros <= 0.833)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[4];
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = upgradesprites[5];
+        }
+        //transform.GetChild(0).GetComponent<SpriteRenderer>().transform.localScale = new Vector3(0.35f, 0.35f, 1f);
+    }
+
 
 }
