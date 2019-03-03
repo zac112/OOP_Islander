a710fe192dd0fda2e778881409e002b345fed4ce
Matti Loimaranta <makalo@utu.fi>
2019-03-03 15:05:12 +0200

    Added music change based on population level as comment

--- Assets/Scripts/Upgrades/PopulationUpgrade.cs
+++ Assets/Scripts/Upgrades/PopulationUpgrade.cs
@@ -7,7 +7,7 @@ public class PopulationUpgrade : Upgrade
     // Start is called before the first frame update
     public void Start()
     {
-        maxLevelModifier = 10;
+        maxLevelModifier = 5;
     }
 
     // Update is called once per frame
@@ -21,10 +21,27 @@ public class PopulationUpgrade : Upgrade
         UpgradeTargets target = UpgradeTargets.population;
         City city = gameObject.GetComponent<City>();
         int currentLevel = city.GetLevel(target);
-
+        
+        /*
+        // Change music based on population level
+        switch (currentLevel)
+        {
+            case 3:
+                EventSystem.EventHappened(EventType.CityBig);
+                break;
+            case 4:
+                EventSystem.EventHappened(EventType.HuntingSmall);
+                break;
+            case 5:
+                EventSystem.EventHappened(EventType.HuntingBig);
+                break;
+            default:
+                break;
+        }
+        */
         if (currentLevel <= city.GetLevel(UpgradeTargets.city) * maxLevelModifier)
         {
-            city.UseResources(GetPrices(currentLevel));
+            city.UseResources(GetPrices(currentLevel + 1));
             city.AddPopulation(currentLevel * 10);
             Debug.Log("population upgraded");
         }
