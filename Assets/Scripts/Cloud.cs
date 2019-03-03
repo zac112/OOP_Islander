0ed3f71ea8a01c0829e5db032d65354a43dcc541
Erno Lokkila <eolokk@utu.fi>
2019-03-03 19:06:55 +0200

    added clouds

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(Random.Range(2f, 5f), Random.Range(2f, 5f), 1);
+    }
+
+    void Update()
+    {
+        float x = transform.position.x;
+        transform.position = new Vector3(x-0.1f, transform.position.y, transform.position.z);
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cloudPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("StartSpawning");   
+    }
+
+    private IEnumerator StartSpawning() {
+        while (true)
+        {
+            GameObject go = Instantiate<GameObject>(cloudPrefab);
+            Vector3 min = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, -10));
+            Vector3 max = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, -10));
+            go.transform.position = new Vector3(max.x+10, Random.Range(min.y,max.y),-5);
+            yield return new WaitForSeconds(Random.Range(2, 7));
+        }
+    }
+}
