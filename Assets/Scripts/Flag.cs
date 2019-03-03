96c7cad42ad3b3702dc0541537982467e080f649
Erno Lokkila <eolokk@utu.fi>
2019-03-03 22:01:07 +0200

    Flag wobbles

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -7,6 +7,10 @@ public class Flag : MonoBehaviour
     [SerializeField]
     private GameObject Worker;
 
+    private void Start()
+    {
+        StartCoroutine("resize");
+    }
     public void SetWorker(GameObject w)
     {
         this.Worker = w;
@@ -14,5 +18,16 @@ public class Flag : MonoBehaviour
     public GameObject GetWorker()
     {
         return this.Worker;
+    }
+
+    private IEnumerator resize() {
+        int multiplier = 1;
+        while (true) {
+            for (int i = 0; i < 100; i++) {
+                yield return new WaitForSeconds(0.01f);
+                transform.localScale = transform.localScale + new Vector3(0.01f,0.01f,0)*multiplier;
+            }
+            multiplier *= -1;
+        }
     }
 }
